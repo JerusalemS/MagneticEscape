28c2f31f5f7cc9d5fc170aea329157170d747d0e
Jerusalem Sintayehu <94401189+JerusalemS@users.noreply.github.com>
2025-03-04 16:02:49 -0800

    Update C-F-ionloss.py

--- C-F-ionloss.py
+++ C-F-ionloss.py
@@ -1,19 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from astropy import units as u
 
 # Constants
-Q0_cf_H = 7.7e25  # s^-1
-r_IMB = 7647e3  # in meters
-r_exo = 6871e3  
-n_sw = 6e6  # m^-3
-v_sw = 4e5  # m/s
-mu_0 = 4 * np.pi * 1e-7  # Permeability of free space in H/m
-Omega_pc_E = 0.63  # solid angle for Earth
-prot_mass = (1.67e-27) 
+Q0_cf_H = 7.7e25
+r_IMB = 7647000 *(u.m) 
+r_exo = 6871000 *(u.m)
+n_sw = 6e6 * (1/u.m**3)
+v_sw = 4e5 * (u.m/u.s) 
+magnetic_p = (4 * np.pi * (1e-7)) * (u.H / u.m) 
+Omega_pc_E = 0.63  
+prot_mass = (1.67e-27) * (u.kg)
 
 
-# Magnetic dipole moments range
-magnetic_moments = np.logspace(16, 28, 100)
+magnetic_moments = np.logspace(16, 28, 10000) * (u.A * u.m**2)
 
 # Lists to store results
 R_mp_values = []
@@ -22,36 +22,34 @@ escape_rate_values = []
 
 # Calculating values
 for magnetic_moment in magnetic_moments:
-    # Calculate magnetopause radius
-    R_mp = ((mu_0 * magnetic_moment**2) / 
-             (8 * (np.pi**2) * n_sw * (v_sw**2)* prot_mass))**(1/6)
+    R_mp = (((magnetic_p * (magnetic_moment**2)*(1.16**2)) / 
+             (8 * (np.pi**2) * n_sw * (v_sw**2)*prot_mass))**(1/6)).decompose()
 
-    R_mp_values.append(R_mp)
+    R_mp_values.append(R_mp.value)
 
-    # Check if R_mp is calculated correctly
-    print(f"Magnetic Moment: {magnetic_moment:.2e}, R_mp: {R_mp:.2e}")
-
-    # Calculate solid angle Omega_pc
-    if r_IMB <= R_mp:
-        temp_value = 1 - (r_exo / R_mp)
-        Omega_pc = 4 * np.pi * (1 - np.sqrt(temp_value)) if temp_value >= 0 else 0
+    # Solid angle Omega_pc
+    
+    if R_mp >= r_IMB:
+        temp_value = max(0, 1 - (r_exo / R_mp))
+        Omega_pc = 4 * np.pi * (1 - np.sqrt(temp_value))
     else:
-        Omega_pc = 0  
+        Omega_pc = 0
+ 
 
     Omega_pc_values.append(Omega_pc)
-    # Calculate escape rate
-    escape_rate = Q0_cf_H * ((1 - Omega_pc / (4 * np.pi))/(1 - Omega_pc_E / (4 * np.pi)))
+
+    escape_rate = Q0_cf_H * ((1 - (Omega_pc / (4 * np.pi)))/(1 - (Omega_pc_E / (4 * np.pi))))
     escape_rate_values.append(escape_rate)
 
-# Plotting
 plt.figure(figsize=(10, 6))
-plt.plot(magnetic_moments, escape_rate_values, label='Qcf,H', color='blue')
-plt.xscale('log')  # Optional: log scale for x-axis
-plt.yscale('log')  # Optional: log scale for y-axis
+plt.loglog(magnetic_moments, escape_rate_values, label='Qcf,H', color='blue') 
 plt.title('Cross-Field Ion Loss Escape Rates vs. Magnetic Dipole Moments')
-plt.xlabel('Magnetic Dipole Moment (m_dp) [kg m^3/s^2]')
+plt.xlabel('Magnetic Dipole Moment  [Am^2]')
 plt.ylabel('Escape Rate (Qcf,H) [s^-1]')
 plt.grid(True, which="both", ls="--")
 plt.legend()
 plt.show()
-#check
+
+
+
+
